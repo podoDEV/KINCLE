38e9374bcd50d09f1e31d31d539b73e853f73fc9
Zedd0202 <ch41150@hanmail.net>
2020-02-08 11:46:48 +0900

    SwiftUI 로그인 뷰 완성

---
+++ KINCLE/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  KINCLE
+//
+//  Created by Zedd on 2020/02/08.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
--- KINCLE/ViewController.swift
+++ KINCLE/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  KINCLE
-//
-//  Created by Zedd on 2020/02/01.
-//  Copyright © 2020 Zedd. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
-
-
 }
 
