c66eb590d608f44efed3621a8dd01df3074e8312
Zedd0202 <ch41150@hanmail.net>
2020-02-09 16:28:10 +0900

    add Sign In with Apple

--- KINCLE/LoginView.swift
+++ KINCLE/LoginView.swift
@@ -1,17 +1,32 @@
-//
-//  LoginView.swift
-//  KINCLE
-//
-//  Created by Zedd on 2020/02/08.
-//  Copyright © 2020 Zedd. All rights reserved.
-//
 
 import SwiftUI
 import AuthenticationServices
 
+struct SignInWithAppleButtonView: UIViewRepresentable {
+    
+    typealias UIViewType = UIView
+    
+    func makeUIView(context: Context) -> UIView {
+        return ASAuthorizationAppleIDButton()
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        //
+    }
+}
+
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SignInWithAppleButtonView()
+            .frame(width: 280, height: 60, alignment: .center)
+            .onTapGesture { self.showAppleLogin() }
+    }
+    
+    private func showAppleLogin() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.email, .fullName]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.performRequests()
     }
 }
 
