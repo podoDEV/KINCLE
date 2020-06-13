c8ca5483efb540cd1138bcc97175d74252311920
Zedd0202 <ch41150@hanmail.net>
2020-06-13 11:04:16 +0900

    로그인 화면 작업 중.

---
+++ KINCLE/Login/LoginViewController.swift
@@ -0,0 +1,114 @@
+//
+//  LoginViewController.swift
+//  KINCLE
+//
+//  Created by Zedd on 2020/05/16.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import AuthenticationServices
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var headerTopSpace: NSLayoutConstraint!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var forgotInfoButton: UIButton!
+    
+    @IBOutlet weak var appleLoginContainerView: UIView!
+    var keyboardFrame: CGRect = .zero
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupTextFields()
+        self.setupLoginButton()
+        self.setupAppleLoginButton()
+        self.setupButtons()
+        self.registerNofitifcations()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
+    func setupTextFields() {
+        [self.idTextField, self.passwordTextField].forEach {
+            $0?.borderStyle = .none
+        }
+        
+        self.idTextField.placeholder = "아이디를 입력하세요."
+        self.passwordTextField.placeholder = "비밀번호를 입력하세요."
+        self.passwordTextField.isSecureTextEntry = true
+    }
+    
+    func setupLoginButton() {
+        self.loginButton.setAttributedTitle(NSAttributedString(string: "로그인", attributes: [
+            .font: UIFont.systemFont(ofSize: 16, weight: .bold),
+            .foregroundColor: UIColor.white
+        ]), for: .normal)
+        self.loginButton.backgroundColor = UIColor(hex: "#dbdbdb")
+        self.loginButton.layer.cornerRadius = 12
+    }
+    
+    func setupAppleLoginButton() {
+        let button = ASAuthorizationAppleIDButton()
+        self.appleLoginContainerView.addSubview(button)
+        self.appleLoginContainerView.clipsToBounds = true
+        button.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+        self.appleLoginContainerView.layer.cornerRadius = 12
+    }
+    
+    func setupButtons() {
+        let signUpTitle = NSAttributedString(string: "회원가입", attributes: [
+            .font: UIFont.systemFont(ofSize: 14, weight: .light),
+            .foregroundColor: UIColor(hex: "#9b9b9b"),
+            .underlineColor: UIColor(hex: "#9b9b9b"),
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ])
+        
+        let forgotTitle = NSAttributedString(string: "아이디/비밀번호 찾기", attributes: [
+            .font: UIFont.systemFont(ofSize: 14, weight: .light),
+            .foregroundColor: UIColor(hex: "#9b9b9b"),
+            .underlineColor: UIColor(hex: "#9b9b9b"),
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ])
+        self.signUpButton.setAttributedTitle(signUpTitle, for: .normal)
+        self.forgotInfoButton.setAttributedTitle(forgotTitle, for: .normal)
+    }
+    
+    func registerNofitifcations() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            self.keyboardFrame = keyboardFrame
+        }
+        self.headerTopSpace.constant = 30
+        let rate = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+        UIView.animate(withDuration: rate ?? 0.25, animations: {
+            self.view.layoutIfNeeded()
+        })
+        
+    }
+
+    @objc func keyboardWillHide(_ notification: Notification) {
+        self.headerTopSpace.constant = 117
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+}
--- KINCLE/Supporting Files/SceneDelegate.swift
+++ KINCLE/Supporting Files/SceneDelegate.swift
@@ -18,10 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         //guard let _ = (scene as? UIWindowScene) else { return }
-        let mainViewController = MainTabViewController()
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        let loginViewController = storyboard.instantiateViewController(identifier: "LoginViewController") as! LoginViewController
+        //let mainViewController = MainTabViewController()
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = mainViewController
+            window.rootViewController = loginViewController
             self.window = window
             window.makeKeyAndVisible()
         }
