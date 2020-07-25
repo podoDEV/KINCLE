2b54bba4fe7491e3764ebebf206df36ca663fe25
Zedd0202 <ch41150@hanmail.net>
2020-07-25 11:39:55 +0900

    Apply .gitignore

--- KINCLE/Network/ApiManager.swift
+++ KINCLE/Network/ApiManager.swift
@@ -2,11 +2,21 @@
 import Combine
 import UIKit
 
+enum OAuthType {
+    
+    case email
+    case apple
+}
+
 class ApiManager {
         
     static var shared: ApiManager = ApiManager()
     
     let apiHost = "http://13.125.45.92:8080"
+    
+    func login(email: String, password: String, type: OAuthType) {
+       // let url =
+    }
     
 //    @Published var favoriteGyms: [Gym] = []
 //
